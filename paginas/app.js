86c71fefea772bc01de409b4feee51cc8be9812d
UrielGarmendia <garmendiauriel@gmail.com>
2022-06-17 13:31:03 -0300

    Organize los archivos

---
+++ paginas/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const publicFolderPath = path.join(__dirname, './public');
+app.use( express.static(publicFolderPath) );
+
+app.use(express.static(path.join(__dirname, './public')));
+
+app.listen(8000, () => {
+    console.log('servidor funcionando');
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home-page.html'));
+});
+
+app.get('/home-shop', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/home-shop.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/carrito', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/carrito.html'));
+});
+
+app.get('/detalle-producto', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/detalle-producto.html'));
+});
