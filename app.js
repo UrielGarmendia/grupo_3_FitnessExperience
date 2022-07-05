0f4fe497ec0e06935600051601018fd3a63c0d04
agustin alsina <alsina.agustin@gmail.com>
2022-07-05 18:35:10 -0300

    reorganizacion de carpetas y archivos

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.set('view engine', 'ejs');
 
 const publicFolderPath = path.join(__dirname, './public');
 app.use( express.static(publicFolderPath) );
+
+
 
 app.use(express.static(path.join(__dirname, './public')));
 
