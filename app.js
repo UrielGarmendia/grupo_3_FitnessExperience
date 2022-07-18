71fef00f52eb2383013de561d4ff23d3e55d8e18
JuanBogliacino <juanboglia12@gmail.com>
2022-07-18 19:13:02 -0300

    agrfegué method override

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.use( express.static(publicFolderPath) );
