bdfb68e5f2c4d764c59433559778ff3df8b424a0
UrielGarmendia <garmendiauriel@gmail.com>
2022-09-12 19:52:27 -0300

    agregué las correccione que me dijo tincho)

--- controllers/mainControllers.js
+++ controllers/mainControllers.js
@@ -1,8 +1,6 @@
 const fs = require ("fs")
 const path = require('path');
 const db = require('../database/models');
-const productsListPath = path.join(__dirname,"../structure.sql");
-const productsList = fs.readFileSync(productsListPath);
 
 
 
