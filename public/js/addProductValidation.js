c431e3aebe9b5f3dbdd6b64b58106224fd4b520c
agustin alsina <alsina.agustin@gmail.com>
2022-09-26 17:34:04 -0300

    agregando validaciones front

--- public/js/addProductValidation.js
+++ public/js/addProductValidation.js
@@ -1,5 +1,44 @@
-let form = document.querySelector("form")
+window.addEventListener("load", function () {
+  let form = document.querySelector("form");
 
-let prueba = form.prueba
+  form.addEventListener("submit", function (e) {
+    let errores = [];
 
-console.log(prueba)
+    let nombreProducto = document.querySelector(".nombreProducto");
+
+    if (nombreProducto.value == "") {
+      errores.push("Debes asignar un NOMBRE al producto");
+    } else if (nombreProducto.value.lenght < 5) {
+      errores.push(
+        "Debes asignar un NOMBRE de al menos 5 caracteres al producto"
+      );
+    }
+
+    let precioProducto = document.querySelector(".precioProducto");
+
+    if (precioProducto.value == "") {
+      errores.push("Debes asignar un PRECIO al producto");
+    }
+
+    let descripcionProducto = document.querySelector(".descripcionProducto");
+
+    if(descripcionProducto.value == ""){
+        errores.push("Debes realizar una descripcion del producto")
+    } else if (descripcionProducto.value.lenght < 20) {
+      errores.push(
+        "Debes realizar una DESCRIPCION de al menos 20 caracteres del producto"
+      );
+
+    let imagenProducto = document.querySelector(".imagen-producto");
+
+
+
+    } else if (errores.lenght > 0) {
+      e.preventDefault();
+      let ulErrores = document.querySelector(".errores ul");
+      errores.forEach((error) => {
+        ulErrores.innerHTML += `<li>${error}</li>`;
+      });
+    }
+  });
+});
