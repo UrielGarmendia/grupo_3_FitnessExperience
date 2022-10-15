7f118a04cdc5d80c14e35565a965a8bf1c4dab61
agustin alsina <alsina.agustin@gmail.com>
2022-10-15 19:37:43 -0300

    aguegue las rutas para los endpoint de la api

--- app.js
+++ app.js
@@ -9,6 +9,8 @@ const methodOverride = require('method-override');
 const mainRoutes = require('./routes/mainRoutes');
 const productsRoutes = require('./routes/productsRoutes.js');
 const usersRoutes = require('./routes/usersRoutes.js');
+const productsApiRoutes = require("./routes/api/productsApiRoutes");
+const usersApiRoutes = require("./routes/api/usersApiRoutes");
 
 const PORT = process.env.PORT || 8000;
 const publicFolderPath = path.join(__dirname, './public');
@@ -35,9 +37,15 @@ app.use(methodOverride('_method'));
 
 app.use( express.static(publicFolderPath) );
 
+//index
 app.use('/', mainRoutes);
+//products
 app.use("/products", productsRoutes)
+//users
 app.use("/users",usersRoutes)
+//Api´s
+app.use("/api/products",productsApiRoutes)
+app.use("/api/users",usersApiRoutes)
 
 app.listen(PORT, () => {
     console.log('servidor funcionando http://localhost:' + PORT);
